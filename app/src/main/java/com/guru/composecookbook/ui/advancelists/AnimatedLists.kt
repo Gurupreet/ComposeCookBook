13bfe38b9272d0f676a5abc72b0d6d9c7dd8c0f6
Gurupreet <gurpreet.usit@gmail.com>
2020-10-24 20:43:46 +0800

    animated lists

--- app/src/main/java/com/guru/composecookbook/ui/advancelists/AnimatedLists.kt
+++ app/src/main/java/com/guru/composecookbook/ui/advancelists/AnimatedLists.kt
@@ -1,15 +1,16 @@
 package com.guru.composecookbook.ui.advancelists
 
+import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.animatedFloat
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.spring
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Icon
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.Text
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.preferredSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumnFor
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.icons.Icons
@@ -23,8 +24,10 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.imageResource
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.interpolator.view.animation.FastOutLinearInInterpolator
 import com.guru.composecookbook.data.DemoDataProvider
 import com.guru.composecookbook.data.model.Tweet
 import com.guru.composecookbook.theme.typography
@@ -34,11 +37,11 @@ import com.guru.composecookbook.ui.utils.VerticalGrid
 @Composable
 fun AnimatedLists() {
     val tweets = DemoDataProvider.tweetList
-    val animations = listOf("Fade", "ScaleX", "ScaleY", "Bounce")
+    val animations = listOf("Fade", "Scale", "Slide", "Fade+Slide", "Slide up", "RotateX")
 
     Column {
         var selectedIndex by remember { mutableStateOf(0) }
-        VerticalGrid(columns = 4, modifier = Modifier.padding(vertical = 12.dp)) {
+        VerticalGrid(columns = 3, modifier = Modifier.padding(vertical = 12.dp)) {
             animations.forEachIndexed { index, title ->
                 YoutubeChip(
                     selected = index == selectedIndex,
@@ -57,24 +60,94 @@ fun AnimatedLists() {
 
 @Composable
 fun AnimatedListItem(tweet: Tweet, selectedIndex: Int) {
-    var animatedProgress = animatedFloat(0f)
-
-    onActive {
-        animatedProgress.animateTo(
-            targetValue = 1f,
-            anim = tween(1000)
-        )
-    }
 
     val animatedModifier = when (selectedIndex) {
-        0 -> Modifier.padding(8.dp).drawOpacity(animatedProgress.value)
-        1 -> Modifier.padding(8.dp).drawLayer(scaleX = animatedProgress.value)
-        2 -> Modifier.padding(8.dp).drawLayer(scaleY = animatedProgress.value)
-        3 -> {
+        0 -> {
+            val animatedProgress = animatedFloat(0f)
+            onActive {
+                animatedProgress.animateTo(
+                    targetValue = 1f,
+                    anim = tween(600)
+                )
+            }
+            Modifier.padding(8.dp).drawOpacity(animatedProgress.value)
+        }
+        1 -> {
+            val animatedProgress = animatedFloat(0.8f)
+            onActive {
+                animatedProgress.animateTo(
+                    targetValue = 1f,
+                    anim = tween(300, easing = LinearEasing)
+                )
+            }
             Modifier.padding(8.dp)
                 .drawLayer(scaleY = animatedProgress.value, scaleX = animatedProgress.value)
         }
-        else -> Modifier.padding(8.dp).drawOpacity(animatedProgress.value)
+        2 -> {
+            val animatedProgress = animatedFloat(300f)
+            onActive {
+                animatedProgress.animateTo(
+                    targetValue = 0f,
+                    anim = tween(300, easing = FastOutSlowInEasing)
+                )
+            }
+            Modifier.padding(8.dp).drawLayer(translationX = animatedProgress.value)
+        }
+        3 -> {
+            val animatedProgress = animatedFloat(-300f)
+            val opacityProgress = animatedFloat(0f)
+            onActive {
+                animatedProgress.animateTo(
+                    targetValue = 0f,
+                    anim = tween(300, easing = LinearEasing)
+                )
+                opacityProgress.animateTo(
+                    targetValue = 1f,
+                    anim = tween(600)
+                )
+            }
+            Modifier.padding(8.dp)
+                .drawLayer(translationX = animatedProgress.value)
+                .drawOpacity(opacityProgress.value)
+        }
+        4 -> {
+            val animatedProgress = animatedFloat(300f)
+            val opacityProgress = animatedFloat(0f)
+            onActive {
+                animatedProgress.animateTo(
+                    targetValue = 0f,
+                    anim = tween(300, easing = LinearEasing)
+                )
+                opacityProgress.animateTo(
+                    targetValue = 1f,
+                    anim = tween(600)
+                )
+            }
+            Modifier.padding(8.dp)
+                .drawLayer(translationY = animatedProgress.value)
+                .drawOpacity(opacityProgress.value)
+        }
+        5 -> {
+            val animatedProgress = animatedFloat(0f)
+            onActive {
+                animatedProgress.animateTo(
+                    targetValue = 360f,
+                    anim = tween(400, easing = FastOutSlowInEasing)
+                )
+            }
+            Modifier.padding(8.dp)
+                .drawLayer(rotationX = animatedProgress.value)
+        }
+        else -> {
+            val animatedProgress = animatedFloat(0.8f)
+            onActive {
+                animatedProgress.animateTo(
+                    targetValue = 1f,
+                    anim = tween(300)
+                )
+            }
+            Modifier.padding(8.dp).drawOpacity(animatedProgress.value)
+        }
     }
     Row(
         modifier = animatedModifier,
