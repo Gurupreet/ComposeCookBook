711c0ca004ced510ebc7d234acc943e9fe6e3c76
Subash <blacpythoz@gmail.com>
2020-11-26 21:54:17 +0545

    gmail bottom nav bar & badge experiment

--- app/src/main/java/com/guru/composecookbook/ui/demoui/gmail/home/GmailHome.kt
+++ app/src/main/java/com/guru/composecookbook/ui/demoui/gmail/home/GmailHome.kt
@@ -1,18 +1,29 @@
 package com.guru.composecookbook.ui.demoui.gmail.home
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Call
 import androidx.compose.material.icons.outlined.Edit
+import androidx.compose.material.icons.outlined.Mail
+import androidx.compose.material.icons.outlined.MailOutline
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.VectorAsset
 import androidx.compose.ui.platform.DensityAmbient
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -65,8 +76,71 @@ fun GmailHome(navController: NavHostController) {
         scaffoldState = scaffoldState,
         bodyContent = {
             GmailContent(fabExpandState, scaffoldState, navController)
-        }
+        },
+        bottomBar = {
+            BottomNavigation(
+                backgroundColor = Color.White
+            ) {
+
+                BottomNavigationItem(
+                    icon = {
+                        IconWithBadge(badge = 1, icon = Icons.Outlined.Mail)
+                    },
+                    onClick = {
+                    },
+                    selected = true,
+                    label = { Text("Mail") },
+                    alwaysShowLabels = true
+                )
+
+
+                BottomNavigationItem(
+
+                    icon = {
+
+                        IconWithBadge(badge = 0, icon = Icons.Outlined.Call)
+
+                    },
+                    onClick = {
+                    },
+                    selected = true,
+                    label = { Text("Meet") },
+                    alwaysShowLabels = true
+                )
+
+
+            }
+        },
     )
+}
+
+@Composable
+fun IconWithBadge(badge: Int, icon: VectorAsset, modifier: Modifier = Modifier) {
+
+    Box(modifier = Modifier.preferredSize(36.dp)) {
+        Icon(
+            asset = icon,
+            modifier = modifier.align(
+                Alignment.BottomCenter
+            )
+        )
+
+        if (badge != 0) {
+            Text(
+                text = "$badge",
+                textAlign = TextAlign.Center,
+                fontSize = 12.sp,
+                color = MaterialTheme.colors.surface,
+                modifier = Modifier
+                    .padding(top = 4.dp)
+                    .clip(CircleShape)
+                    .background(Color.Red)
+                    .align(Alignment.TopEnd)
+                    .preferredSize(16.dp)
+            )
+        }
+    }
+
 }
 
 
