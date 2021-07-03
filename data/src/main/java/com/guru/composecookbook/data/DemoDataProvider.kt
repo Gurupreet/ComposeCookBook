7877889eb381c793f4144083ed22d6a85059e363
wiryadev <abrarwiryawan@gmail.com>
2021-07-03 14:16:29 +0700

    refactor mutable to immutable variable

--- data/src/main/java/com/guru/composecookbook/data/DemoDataProvider.kt
+++ data/src/main/java/com/guru/composecookbook/data/DemoDataProvider.kt
@@ -344,7 +344,7 @@ object DemoDataProvider {
         HomeScreenItems.PullRefresh,
     )
 
-    val longText =
+    const val longText =
         "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae"
 
     val demoUiList = listOf(
