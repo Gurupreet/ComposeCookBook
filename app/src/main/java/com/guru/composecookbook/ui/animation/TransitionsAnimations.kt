e9d87ce5f3dbda5225291cc58718e424b5793b4b
wiryadev <abrarwiryawan@gmail.com>
2021-07-03 13:45:45 +0700

    Add label for animation preview

--- app/src/main/java/com/guru/composecookbook/ui/animation/TransitionsAnimations.kt
+++ app/src/main/java/com/guru/composecookbook/ui/animation/TransitionsAnimations.kt
@@ -46,10 +46,11 @@ fun MultiStateColorPositionAnimation() {
     val midColor = purple
     val endColor = orange
 
-    val transition = updateTransition(targetState = animationState)
+    val transition = updateTransition(targetState = animationState, label = "transition")
 
     val animatedColor by transition.animateColor(
-        transitionSpec = { tween(500) }
+        transitionSpec = { tween(500) },
+        label = "animatedColor",
     ) { state ->
         when (state) {
             MyAnimationState.START -> startColor
@@ -59,7 +60,8 @@ fun MultiStateColorPositionAnimation() {
     }
 
     val position by transition.animateDp(
-        transitionSpec = { tween(500) }
+        transitionSpec = { tween(500) },
+        label = "position",
     ) { state ->
         when (state) {
             MyAnimationState.START -> 0.dp
