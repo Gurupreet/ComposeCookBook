f490ccf46acb8bc08852a1b39064b268a0414014
wirya <abrarwiryawan@gmail.com>
2021-07-24 23:00:26 +0700

    optimize import for MotionLayoutDemo.kt

--- app/src/main/java/com/guru/composecookbook/ui/home/motionlayout/MotionLayoutDemo.kt
+++ app/src/main/java/com/guru/composecookbook/ui/home/motionlayout/MotionLayoutDemo.kt
@@ -6,33 +6,18 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
-import androidx.compose.material.Icon
 import androidx.compose.material.Text
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Close
-import androidx.compose.material.icons.filled.PlayArrow
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.layoutId
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import androidx.compose.ui.zIndex
 import androidx.constraintlayout.compose.ConstraintSet
 import androidx.constraintlayout.compose.MotionLayout
-import androidx.constraintlayout.compose.layoutId
 import com.guru.composecookbook.data.AlbumsDataProvider
-import com.guru.composecookbook.data.DemoDataProvider
-import com.guru.composecookbook.moviesapp.ui.home.components.imageIds
-import com.guru.fontawesomecomposelib.FaIcon
 
 @Preview(group = "motion7")
 @Composable
