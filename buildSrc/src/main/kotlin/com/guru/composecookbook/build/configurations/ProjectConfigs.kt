ffa35e51384c643f45c63400608d19713b38d439
Gurupreet <gurpreet.usit@gmail.com>
2025-06-22 16:39:21 +0200

    Update all Compose and Material dependencies

--- buildSrc/src/main/kotlin/com/guru/composecookbook/build/configurations/ProjectConfigs.kt
+++ buildSrc/src/main/kotlin/com/guru/composecookbook/build/configurations/ProjectConfigs.kt
@@ -3,9 +3,9 @@ package com.guru.composecookbook.build.configurations
 import com.guru.composecookbook.build.dependencies.Versions
 
 object ProjectConfigs {
-    const val compileSdkVersion = 34
-    const val minSdkVersion = 25
-    const val targetSdkVersion = 34
+    const val compileSdkVersion = 34  // Latest supported Android SDK version
+    const val minSdkVersion = 25  // Original minSdkVersion
+    const val targetSdkVersion = 34  // Matching compileSdkVersion
     const val applicationId = "com.guru.composecookbook"
     const val kotlinCompilerExtensionVersion = Versions.composeCompiler
 }
